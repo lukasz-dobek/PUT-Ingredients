36307f81684db2e0b2cd5f19a9fdedd1841b2b09
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-04-25 19:59:17 +0200

    Register and login page render - basic register logic

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,42 @@ var express = require('express');
 var router = express.Router();
 const pgClient = require('./../db/pg-controller');
 
-router.get('/', (req, res) => {
+router.get('/login', function(req, res, next) {
+    res.render('login');
+  });
+  
+  router.get('/register', function(req, res, next) {
+    res.render('register');
+  });
+  
+  router.post('/register', (req, res, next) => {
+    let email = req.body.email;
+    let nickname = req.body.nickname;
+    let password = req.body.password;
+    let passwordConfirm = req.body.passwordconfirm;
+    let name = req.body.name;
+    let surname = req.body.surname;
+  
+    console.log(`${email}, ${nickname}`);
+  
+    const insertQueryString = `INSERT INTO users (email_address, password, nickname, date_of_join, name, surname, is_admin, state, activation_url, url_status, salt) VALUES ($1, $2, $3, to_timestamp(${Date.now()/1000.0}), $4, $5, '0', 4, 'abc', '0', 'abcdefghj');`
+    pgClient.query(insertQueryString, [email, password, nickname, name, surname], (err, result) =>{
+        if (err) {
+            throw err;
+            res.render('error');
+        }
+        console.log(result);
+    })
+    res.redirect('/users/login');
+  });
+
+router.get('/api/all', (req, res) => {
     pgClient.query('SELECT * FROM users', (err, result) => {
         res.json(result.rows);
     });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/api/:id', (req, res) => {
     const queryString = 'SELECT * FROM users WHERE id_user = $1';
     const value = [parseInt(req.params.id)];
 
@@ -17,4 +46,5 @@ router.get('/:id', (req, res) => {
         res.json(result.rows);
     });
 });
+
 module.exports = router;
