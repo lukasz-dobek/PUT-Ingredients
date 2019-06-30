f26a2112a981db89b497aed5e7c31da30c2714e4
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-07-01 01:01:21 +0200

    Reorder of menu items; favouritesHandler basic icon coloring

---
+++ public/javascripts/favouritesHandler.js
@@ -0,0 +1,6 @@
+function action(e){
+    let heart = document.getElementById(e.target.id);
+    heart.classList = [];
+    heart.classList.add('fas', 'fa-heart');
+    heart.style = "color: red; font-size: 1.3vw;";
+}
