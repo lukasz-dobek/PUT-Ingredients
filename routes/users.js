5cf6088ca23e4c102db89e093cfcce205ad0fa86
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-07-01 00:19:01 +0200

    User settings; favourites shell

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const crypto = require('crypto');
 const passport = require('passport');
 const { ensureLoggedIn } = require('../config/auth');
 
-router.get('/user_settings', (req, res) => {
+router.get('/settings', (req, res) => {
     res.render('./users/user_settings');
 });
 
@@ -39,10 +39,44 @@ router.get('/user_recipes/:user', (req, res) => {
         if(searchUserRecipesQueryError) {
             throw searchUserRecipesQueryError;
         }
-        console.log();
         res.render('./users/user_recipes', { recipes: searchUserRecipesQueryResult.rows });
     });
 });
+
+router.get('/favourites', (req, res) => {
+    const userFavouritesQueryString = `
+    SELECT 
+        rec.id_recipe, 
+        rec.recipe_name, 
+        rec.score, 
+        TO_CHAR(rec.date_of_creation, 'DD/MM/YYYY') AS date_of_creation, 
+        rec.complicity, 
+        rec.preparation_time, 
+        rec.description, 
+        rec.number_of_people, 
+        rec.link_to_recipe, 
+        rec.photo_one, 
+        rec.photo_two, 
+        rec.photo_three, 
+        rec.photo_four,
+        usr.email_address,
+        usr.nickname
+    FROM favourites fav 
+        INNER JOIN recipes rec ON fav.recipe_id = rec.id_recipe
+		INNER JOIN users usr ON usr.id_user = fav.user_id 
+    WHERE usr.nickname = $1;`;
+
+    const userNickname = res.locals.userNickname;
+
+    pgClient.query(userFavouritesQueryString, [userNickname], (userFavouritesQueryError, userFavouritesQueryResult) => {
+        if(userFavouritesQueryError) {
+            throw userFavouritesQueryError;
+        }
+        console.log(userFavouritesQueryResult.rows);
+        res.render('./users/favourites', { recipes: userFavouritesQueryResult.rows });
+    });
+});
+
 
 router.get('/shopping_lists', (req, res) => {
     res.render('./users/shopping_lists');
