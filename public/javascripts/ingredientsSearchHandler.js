cb0c6ede825b4fd0c48922df2dd875d037f3559d
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-07-07 22:41:47 +0200

    Ingredients search screen

--- public/javascripts/ingredientsSearchHandler.js
+++ public/javascripts/ingredientsSearchHandler.js
@@ -1,8 +1,21 @@
 let ingredientsNumber = 16;
-let quickIngredientsSearch = ['Makaron', 'Ser żółty', 'Pieczywo pszenne', 'Chleb tostowy',
-    'Mleko', 'Pierś z kurczaka', 'Pomidor', 'Ogórek kiszony', 'Ziemniaki',
-    'Ser topiony', 'Przecier pomidorowy', 'Cytryna', 'Musztarda',
-    'Jajka', 'Masło', 'Ser twarogowy'];
+let quickIngredientsSearch = ["Marchew",
+"Pomidor",
+"Jablko",
+"Wołowina",
+"Pieczarki",
+"Krewetki",
+"Łosoś",
+"Cukier",
+"Pierś z kurczaka",
+"Pieczywo",
+"Miod",
+"Twaróg",
+"Jajka",
+"Mleko",
+"Mąka pszenna",
+"Proszek do pieczenia"];
+
 let ingredientsInRow = 4;
 
 let quickIngredientsSearchBox = document.getElementById('quickIngredientsSearch');
@@ -17,18 +30,18 @@ for (let i = 0; i < ingredientsInRow; i++) {
 
         let ingredientFormCheck = document.createElement('div');
         ingredientFormCheck.classList.add('form-check');
-    
+
         let ingredientCheckBox = document.createElement('input');
         ingredientCheckBox.classList.add('form-check-input');
         ingredientCheckBox.name = 'ingredientCheck';
-        ingredientCheckBox.id = `ingredientCheck${(i*4)+j}`;
+        ingredientCheckBox.id = `ingredientCheck${(i * 4) + j}`;
         ingredientCheckBox.type = "checkbox";
-        ingredientCheckBox.value=quickIngredientsSearch[(i*4)+j];
+        ingredientCheckBox.value = quickIngredientsSearch[(i * 4) + j];
 
         let ingredientCheckBoxLabel = document.createElement('label');
         ingredientCheckBoxLabel.classList.add('form-check-label');
         ingredientCheckBoxLabel.htmlFor = ingredientCheckBox.id;
-        ingredientCheckBoxLabel.textContent = quickIngredientsSearch[(i*4)+j];
+        ingredientCheckBoxLabel.textContent = quickIngredientsSearch[(i * 4) + j];
 
         ingredientFormCheck.appendChild(ingredientCheckBox);
         ingredientFormCheck.appendChild(ingredientCheckBoxLabel);
