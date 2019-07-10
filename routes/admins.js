96726a7450ac8315b76b1d56cdcd976287fc4309
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-07-10 20:47:14 +0200

    Admin panel init

--- routes/admins.js
+++ routes/admins.js
@@ -3,7 +3,7 @@ const argon2 = require('argon2');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Udalo sie');
+    res.render('./admin_panel/main', { layout: 'layout_admin_panel'});
 });
 
 module.exports = router;
