e555ac2364e158ff0bda94ec28ce5c4fb0cfaa29
arturbronkax@gmail.com <Reinmar1422!>
2019-07-14 23:05:42 +0200

    Shopping list

--- public/javascripts/shopListHandler.js
+++ public/javascripts/shopListHandler.js
@@ -1,32 +1,32 @@
-function splitRecipeId(elementId) {
-    return elementId.split('_')[1];
-}
-function addToShopList(e, userId) {
-    let shopList = document.getElementById(e.target.id);
-    let recipeId = splitRecipeId(shopList.id);
-
-        $.post("/api/shoppingList/", {
-            userId: userId,
-            recipeId: recipeId,
-        });
-}
-
-function getIdOfList(e) {
-    let shopList = document.getElementById(e.target.id);
-    let recipeId = splitRecipeId(shopList.id);
-    return recipeId;
-
-}
-
-function deleteFromShoppingList(e,userId) {
-    let shopList = document.getElementById(e.target.id);
-    let recipeId = splitRecipeId(shopList.id);
-    $.ajax({
-        url: '/api/shoppingList/',
-        type: 'DELETE',
-        data: {
-            userId: userId,
-            recipeId: recipeId
-        }
-    });
-}
+// function splitRecipeId(elementId) {
+//     return elementId.split('_')[1];
+// }
+// function addToShopList(e, userId) {
+//     let shopList = document.getElementById(e.target.id);
+//     let recipeId = splitRecipeId(shopList.id);
+//
+//         $.post("/api/shoppingList/", {
+//             userId: userId,
+//             recipeId: recipeId,
+//         });
+// }
+//
+// function getIdOfList(e) {
+//     let shopList = document.getElementById(e.target.id);
+//     let recipeId = splitRecipeId(shopList.id);
+//     return recipeId;
+//
+// }
+//
+// function deleteFromShoppingList(e,userId) {
+//     let shopList = document.getElementById(e.target.id);
+//     let recipeId = splitRecipeId(shopList.id);
+//     $.ajax({
+//         url: '/api/shoppingList/',
+//         type: 'DELETE',
+//         data: {
+//             userId: userId,
+//             recipeId: recipeId
+//         }
+//     });
+// }
