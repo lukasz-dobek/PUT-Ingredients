644354018810895e58114f164b749d9ae5d93997
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-07-29 22:27:45 +0200

    graphs routes

--- routes/api/graphs.js
+++ routes/api/graphs.js
@@ -2,8 +2,22 @@ const express = require('express');
 const router = express.Router();
 const pgClient = require('../../db/pg-controller');
 
-router.get('/', (req, res) => {
-    res.send('temp');
+router.get('/recipes_in_month', (req, res) => {
+    const recipesInMonthsQueryString = `
+    SELECT 
+        date_part('day', date_trunc('day', date_of_creation)) AS day,
+        count(*) AS number_of_recipes
+    FROM recipes
+    WHERE date_of_creation > now() - interval '1 month' 
+    GROUP BY 1
+    ORDER BY 1;`;
+    pgClient.query(recipesInMonthsQueryString, (recipesInMonthsQueryError, recipesInMonthsQueryResult) => {
+        if (recipesInMonthsQueryError) {
+            throw recipesInMonthsQueryError;
+        }
+        res.json(recipesInMonthsQueryResult.rows)
+    });
+
 });
 
 
