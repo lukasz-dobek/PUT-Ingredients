16de5392d1031b0ec020c8d2447f456cf8c1a0d4
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-07-30 22:14:16 +0200

    Users activity delete information

--- db/queries/trigger.sql
+++ db/queries/trigger.sql
@@ -135,3 +135,23 @@ AFTER INSERT
 ON users
 FOR EACH ROW
 EXECUTE PROCEDURE register_to_activities();
+
+CREATE OR REPLACE FUNCTION delete_account_to_activities()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+    INSERT INTO user_activities (user_id, date_of_activity, activity_name)
+    VALUES(NEW.id_user, 
+    NEW.date_of_deletion, 
+    'Usunięcie konta'
+    );
+   RETURN NEW;
+END;
+$BODY$
+language plpgsql;
+
+CREATE TRIGGER delete_account_to_activities
+AFTER UPDATE OF date_of_deletion 
+ON users
+FOR EACH ROW
+EXECUTE PROCEDURE delete_account_to_activities();
