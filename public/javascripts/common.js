eae742d2bad1fbb354a98c9e0229ea57e2273596
Lukasz Dobek <lukasz.dobek@outlook.com>
2019-11-17 20:58:14 +0100

    RecipeID splitting function and delete fetch handler in common functions

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -10,6 +10,10 @@ async function getCategoryNames() {
     return categories;
 }
 
+function splitRecipeId(elementId) {
+    return elementId.split('_')[1];
+}
+
 async function postData(url = '', data = {}) {
     // Default options are marked with *
     const response = await fetch(url, {
@@ -27,4 +31,21 @@ async function postData(url = '', data = {}) {
     return await response.json(); // parses JSON response into native JavaScript objects
 }
 
-export { getCategoryNames, postData };
+async function deleteData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json' // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+}
+
+export { getCategoryNames, postData, deleteData, splitRecipeId };
