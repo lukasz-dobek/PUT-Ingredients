be2767b1e3cad321ebcf063fdc6ced0a5bee81ba
Lukasz Dobek <lukasz.dobek@outlook.com>
2020-01-05 23:08:58 +0100

    Update

--- public/javascripts/add-recipe-form-control-vanilla.js
+++ public/javascripts/add-recipe-form-control-vanilla.js
@@ -393,10 +393,8 @@ $.getJSON("/api/recipes/all", (data) => {
         return recipeNameTaken;
     };
 
-    // Validate the field
     let hasError = function (field) {
 
-        // Don't validate submits, buttons, file and reset inputs, and disabled fields
         if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
 
         if (checkRecipeName()) {
@@ -408,63 +406,48 @@ $.getJSON("/api/recipes/all", (data) => {
             if (field.id === 'numberOfPortions' && field.value > 20) return 'Wartość nie może być większa niż 20';
             if (field.value <= 0) return "Wartość nie może być zerowa, lub ujemna."
         }
-        // Get validity
+
         let validity = field.validity;
 
-        // If valid, return null
         if (validity.valid) return;
 
         if (validity.typeMismatch) {
-            // Email
             if (field.type === 'number') return 'Wprowadzone dane muszą być liczbą.';
         }
 
         if(field.id==="mainPhotoSelect"){
             return "Musisz dodać co najmniej jedno zdjęcie i wybrać, które ma być główne";
         }
-        // If field is required and empty
         if (validity.valueMissing) {
             if (field.id === "categorySelect1") return 'Musi zostać wybrana co najmniej jedna kategoria.';
             if (field.name === 'check') return 'Musisz zaakceptować regulamin.';
             return 'To pole jest obowiązkowe.';
         }
-        // If too short
         if (validity.tooShort) {
             if (field.name === 'recipename') return 'Nazwa przepisu musi składać się co najmniej 5 znaków.'
             if (field.name === 'ingredientName') return 'Nazwa składnika musi mieć co najmniej 3 znaki.'
         }
 
-        // If too long
         if (validity.tooLong) {
             if (field.name === 'nickname') return 'Nazwa przepisu może się składać masymalnie z 35 znaków.'
             if (field.name === 'ingredientName') return 'Nazwa składnika może się składać maksymalnie z 50 znaków.'
         }
-        // If all else fails, return a generic catchall error
         return 'Podana wartość jest błędna.';
-
     };
 
-
-    // Show an error message
     let showError = function (field, error) {
-
-        // Add error class to field
         field.classList.add('error');
-        // Get field id or name
+
         let id = field.id || field.name;
         if (!id) return;
 
-        // Check if error message field already exists
-        // If not, create one
         let message = field.form.querySelector('.error-message#error-for-' + id);
         if (!message) {
             message = document.createElement('div');
             message.className = 'error-message container';
             message.id = 'error-for-' + id;
 
-            // If the field is a radio button or checkbox, insert error after the label
             let label;
-            //if (field.type === 'radio' || field.type ==='checkbox') {
             label = field.form.querySelector('label[for="' + id + '"]') || field.parentNode;
             if (label) {
                 if (field.id === 'category1') {
@@ -479,50 +462,31 @@ $.getJSON("/api/recipes/all", (data) => {
                 }
 
             }
-            //}
 
-            // Otherwise, insert it after the field
             if (!label) {
                 field.parentNode.insertBefore(message, field.nextSibling);
             }
 
         }
 
-        // Add ARIA role to the field
         field.setAttribute('aria-describedby', 'error-for-' + id);
-
-        // Update error message
         message.innerHTML = error;
-
-        // Show error message
         message.style.display = 'block';
         message.style.visibility = 'visible';
-        //   }
     };
 
-
-    // Remove the error message
     let removeError = function (field) {
-
-        // Remove error class to field
         field.classList.remove('error');
-
-        // Remove ARIA role from the field
         field.removeAttribute('aria-describedby');
-        // Get field id or name
+
         let id = field.id || field.name;
         if (!id) return;
 
-
-        // Check if an error message is in the DOM
         let message = field.form.querySelector('.error-message#error-for-' + id + '');
         if (!message) return;
-
-        // If so, hide it
         message.innerHTML = '';
         message.style.display = 'none';
         message.style.visibility = 'hidden';
-
     };
 
     document.addEventListener('blur', function (event) {
@@ -534,27 +498,16 @@ $.getJSON("/api/recipes/all", (data) => {
                 error ="";
             }
         }
-        // If there's an error, show it
         if (error) {
             showError(event.target, error);
             return;
         }
-
-        // Otherwise, remove any existing error message
         removeError(event.target);
-
     }, true);
 
-
-    // Check all fields on submit
     document.addEventListener('submit', function (event) {
-
-        // Only run on forms flagged for validation
-        // Get all of the form elements
         let fields = event.target.elements;
 
-        // Validate each field
-        // Store the first field with an error to a letiable so we can bring it into focus leter
         let error, hasErrors;
         for (let i = 0; i < fields.length; i++) {
             if(fields[i].id.includes("mainPhotoSelect")){
@@ -602,15 +555,9 @@ $.getJSON("/api/recipes/all", (data) => {
             }
         }
 
-        // If there are errrors, don't submit form and focus on first element with error
         if (hasErrors) {
             event.preventDefault();
             hasErrors.focus();
         }
-
-        // Otherwise, let the form submit normally
-        // You could also bolt in an Ajax form submit process here
-
     }, false);
-
 });
--- public/javascripts/new-password-validation.js
+++ public/javascripts/new-password-validation.js
@@ -1,137 +1,16 @@
-
-// Validate the field
-let hasError = function (field) {
-
-    // Don't validate submits, buttons, file and reset inputs, and disabled fields
-    if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
-
-    // if(field.name === 'oldPassword'){
-    //     let email_address = field.value;
-    //     if(email_address) {
-    //         // $.getJSON(`/api/users/email/${email_address}`, (jsonData) => {
-    //         //     if(arg)
-    //         // });
-    //     }
-    // }
-
-    if (checkPassw()) {
-        if (field.name === 'passwordConfirm') return 'Hasła nie zgadzają się.';
-    }
-    let validity = field.validity;
-
-    if (validity.valid) return;
-    // If too short
-
-    if (validity.valueMissing) {
-        return 'To pole jest obowiązkowe.';
-    }
-
-    if (validity.tooShort) {
-        if (field.name === 'newPassword') return 'Hasło musi składać się z co najmniej 8 znaków, w tym co najmniej jednej małej litery, jednej litery wielkiej, jednej cyfry oraz znaku specjalnego..'
-    }
-
-    // If pattern doesn't match
-    if (validity.patternMismatch) {
-        if (field.name === 'newPassword') return 'Podane hasło nie spełnia wymagań. Musi składać się ono z co najmniej 8 znaków, w tym co najmniej jednej małej litery, jednej litery wielkiej, jednej cyfry oraz znaku specjalnego.';
-    }
-
-    return 'Podana wartość jest błędna.';
-
-};
-
-
-// Show an error message
-let showError = function (field, error) {
-
-    // Add error class to field
-    field.classList.add('error');
-    // Get field id or name
-    let id = field.id || field.name;
-    if (!id) return;
-
-    // If not, create one
-    let message = field.form.querySelector('.error-message#error-for-' + id);
-    if (!message) {
-        message = document.createElement('div');
-        message.className = 'error-message';
-        //message.style.wordWrap = 'normal;'
-        message.id = 'error-for-' + id;
-
-        // If the field is a radio button or checkbox, insert error after the label
-        let label;
-        //if (field.type === 'radio' || field.type ==='checkbox') {
-        label = field.form.querySelector('label[for="' + id + '"]') || field.parentNode;
-        if (label) {
-            label.parentNode.insertBefore(message, label.nextSibling);
-        }
-        //}
-
-        // Otherwise, insert it after the field
-        if (!label) {
-            field.parentNode.insertBefore(message, field.nextSibling);
-        }
-
-    }
-
-    // Add ARIA role to the field
-    field.setAttribute('aria-describedby', 'error-for-' + id);
-
-    // Update error message
-    message.innerHTML = error;
-
-    // Show error message
-    message.style.marginLeft = '2%';
-    message.style.display = 'block';
-    message.style.visibility = 'visible';
-    //   }
-};
-
-
-// Remove the error message
-let removeError = function (field) {
-
-    // Remove error class to field
-    field.classList.remove('error');
-
-    // Remove ARIA role from the field
-    field.removeAttribute('aria-describedby');
-    // Get field id or name
-    let id = field.id || field.name;
-    if (!id) return;
-
-
-    // Check if an error message is in the DOM
-    let message = field.form.querySelector('.error-message#error-for-' + id + '');
-    if (!message) return;
-
-    // If so, hide it
-    message.innerHTML = '';
-    message.style.display = 'none';
-    message.style.visibility = 'hidden';
-
-
-};
-
 document.addEventListener('blur', function (event) {
-
     let error = hasError(event.target);
+
     if (error) {
         showError(event.target, error);
         return;
     }
 
-    // Otherwise, remove any existing error message
     removeError(event.target);
 }, true);
 
-// Check all fields on submit
 document.addEventListener('submit', function (event) {
-
-    // Only run on forms flagged for validation
-    // Get all of the form elements
     let fields = event.target.elements;
-    // Validate each field
-    // Store the first field with an error to a letiable so we can bring it into focus leter
     let error, hasErrors;
 
     for (let i = 0; i < fields.length; i++) {
@@ -143,15 +22,90 @@ document.addEventListener('submit', function (event) {
             }
         }
     }
-    // Ifthere are errrors, don't submit form and focus on first element with error
+
     if (hasErrors) {
         event.preventDefault();
         hasErrors.focus();
     }
-    // Otherwise, let the form submit normally
-    // You could also bolt in an Ajax form submit process here
 }, false);
 
+let hasError = function (field) {
+
+    if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
+
+    if (checkPassw()) {
+        if (field.name === 'passwordConfirm') return 'Hasła nie zgadzają się.';
+    }
+    let validity = field.validity;
+
+    if (validity.valid) return;
+
+    if (validity.valueMissing) {
+        return 'To pole jest obowiązkowe.';
+    }
+
+    if (validity.tooShort) {
+        if (field.name === 'newPassword') return 'Hasło musi składać się z co najmniej 8 znaków, w tym co najmniej jednej małej litery, jednej litery wielkiej, jednej cyfry oraz znaku specjalnego..'
+    }
+
+    if (validity.patternMismatch) {
+        if (field.name === 'newPassword') return 'Podane hasło nie spełnia wymagań. Musi składać się ono z co najmniej 8 znaków, w tym co najmniej jednej małej litery, jednej litery wielkiej, jednej cyfry oraz znaku specjalnego.';
+    }
+
+    return 'Podana wartość jest błędna.';
+};
+
+let showError = function (field, error) {
+
+    field.classList.add('error');
+    let id = field.id || field.name;
+    if (!id) return;
+
+    let message = field.form.querySelector('.error-message#error-for-' + id);
+    if (!message) {
+        message = document.createElement('div');
+        message.className = 'error-message';
+        message.id = 'error-for-' + id;
+
+        let label;
+        label = field.form.querySelector('label[for="' + id + '"]') || field.parentNode;
+
+        if (label) {
+            label.parentNode.insertBefore(message, label.nextSibling);
+        }
+
+        if (!label) {
+            field.parentNode.insertBefore(message, field.nextSibling);
+        }
+
+    }
+
+    field.setAttribute('aria-describedby', 'error-for-' + id);
+
+    message.innerHTML = error;
+
+    message.style.marginLeft = '2%';
+    message.style.display = 'block';
+    message.style.visibility = 'visible';
+};
+
+let removeError = function (field) {
+
+    field.classList.remove('error');
+    field.removeAttribute('aria-describedby');
+
+    let id = field.id || field.name;
+    if (!id) return;
+
+    let message = field.form.querySelector('.error-message#error-for-' + id + '');
+    if (!message) return;
+
+    message.innerHTML = '';
+    message.style.display = 'none';
+    message.style.visibility = 'hidden';
+
+
+};
 
 let checkPassw = function () {
     let password = document.getElementById('passwordForm2');
@@ -164,5 +118,3 @@ let checkPassw = function () {
         }
     }
 };
-
-//});
